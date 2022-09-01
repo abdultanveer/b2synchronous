70830e0a71a0bcb3e9fa364aa80080f1792f3e04
abdul ansari <abdul.tanveer@gmail.com>
2022-09-01 12:12:16 +0530

    onclick button

--- app/src/main/java/com/example/b2synchronous/HomeActivity.java
+++ app/src/main/java/com/example/b2synchronous/HomeActivity.java
@@ -3,6 +3,8 @@ package com.example.b2synchronous;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void handleClicks(View viewClicked) {
+        Toast.makeText(this,"welcome to android", Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/example/b2synchronous/MainActivity.kt
+++ app/src/main/java/com/example/b2synchronous/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.b2synchronous
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickHandler(viewClicked: View) {
+        Toast.makeText(this,"welcome to android",Toast.LENGTH_SHORT).show()
     }
 }
