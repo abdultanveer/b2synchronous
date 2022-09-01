feb37d52f12fe86350add20181e14ee18e081349
abdul ansari <abdul.tanveer@gmail.com>
2022-09-01 12:31:25 +0530

    onclick button- gettext from edittext

--- app/src/main/java/com/example/b2synchronous/HomeActivity.java
+++ app/src/main/java/com/example/b2synchronous/HomeActivity.java
@@ -4,18 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
-
+    EditText etContact;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        etContact = findViewById(R.id.etContact);
     }
 
     public void handleClicks(View viewClicked) {
-        Toast.makeText(this,"welcome to android", Toast.LENGTH_SHORT).show();
+        String name = etContact.getText().toString();
+        Toast.makeText(this,name, Toast.LENGTH_SHORT).show();
 
     }
 }
--- app/src/main/java/com/example/b2synchronous/MainActivity.kt
+++ app/src/main/java/com/example/b2synchronous/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.b2synchronous
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etName : EditText //declaration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //inflated -layoutinflater
+        etName = findViewById(R.id.etName)
     }
 
     fun clickHandler(viewClicked: View) {
-        Toast.makeText(this,"welcome to android",Toast.LENGTH_SHORT).show()
+       // var name: String = etName.text.toString()
+        var name = etName.text.toString()
+        Toast.makeText(this,name,Toast.LENGTH_SHORT).show()
     }
 }
