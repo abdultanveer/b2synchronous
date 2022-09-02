86b87465246c52f7963ca69d01384ca578efdd67
abdul ansari <abdul.tanveer@gmail.com>
2022-09-02 11:00:20 +0530

    start dialer/alarms

--- app/src/main/java/com/example/b2synchronous/MainActivity.kt
+++ app/src/main/java/com/example/b2synchronous/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.b2synchronous
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.util.Log
 import android.view.View
 import android.widget.EditText
@@ -19,14 +21,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clickHandler(viewClicked: View) {
-       // var name: String = etName.text.toString()
-        Log.e(TAG,"click handler")
+        when(viewClicked.id){
+            R.id.btnLogin -> {startHomeActivity()}
+            R.id.btnDial ->{startDialer()}
+            R.id.btnAlarm -> {createAlarm("sync",10,59)}
+        }
+
+    }
+
+    private fun startDialer() {
+        var dIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:123456"))
+        startActivity(dIntent)
+    }
+
+    private fun startHomeActivity() {
+        Log.e(TAG, "click handler")
         var name = etName.text.toString()
 
-        var hIntent = Intent(this,HomeActivity::class.java)
-        hIntent.putExtra("nkey",name)
+        var hIntent = Intent(this, HomeActivity::class.java)
+        hIntent.putExtra("nkey", name)
         startActivity(hIntent)
     }
+
+    fun createAlarm(message: String, hour: Int, minutes: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+//https://developer.android.com/guide/components/intents-common#kotlin
+
 
     companion object{
         var TAG = MainActivity::class.java.simpleName
