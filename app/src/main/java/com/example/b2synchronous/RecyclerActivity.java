755d253ca49eccb1a7866d42010a9d521a28ed14
abdul ansari <abdul.tanveer@gmail.com>
2022-09-07 11:55:19 +0530

    recyclerview part-3a

--- app/src/main/java/com/example/b2synchronous/RecyclerActivity.java
+++ app/src/main/java/com/example/b2synchronous/RecyclerActivity.java
@@ -1,17 +1,20 @@
 package com.example.b2synchronous;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class RecyclerActivity extends AppCompatActivity {
-    String[] languages = new String[]{"english","hindi","urdu","kannada","tamil","telgu"};
+    String[] languages = new String[]{"english","hindi","urdu","kannada","tamil","telgu","english","hindi","urdu","kannada","tamil","telgu","english","hindi","urdu","kannada","tamil","telgu","english","hindi","urdu","kannada","tamil","telgu","english","hindi","urdu","kannada","tamil","telgu"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
         RecyclerView rView = findViewById(R.id.languagesRview);
         LangsAdapter adapter = new LangsAdapter(languages);
+        rView.setLayoutManager(new LinearLayoutManager(this));
+        rView.setAdapter(adapter);
     }
 }
