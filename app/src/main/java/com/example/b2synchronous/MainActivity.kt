3075662cd94f268e1ce2737978aa3b55ddc9b137
abdul ansari <abdul.tanveer@gmail.com>
2022-09-12 16:27:33 +0530

    notifications

--- app/src/main/java/com/example/b2synchronous/MainActivity.kt
+++ app/src/main/java/com/example/b2synchronous/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.b2synchronous
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock
@@ -11,6 +16,8 @@ import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.b2synchronous.model.Student
 
 class MainActivity : AppCompatActivity() {
@@ -35,9 +42,56 @@ class MainActivity : AppCompatActivity() {
             R.id.btnDial ->{startDialer()}
             R.id.btnAlarm -> {createAlarm("sync",10,59)}
             R.id.btnDownload -> {downloadImage()}
+            R.id.btnNotify -> {showNotification()}
         }
         print("outside")
     }
+
+    private fun showNotification() {
+        createNotificationChannel()
+        val intent = Intent(this, HomeActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        var builder = NotificationCompat.Builder(this, "CHANNEL_ID")
+            .setSmallIcon(com.google.android.material.R.drawable.ic_clock_black_24dp)
+            .setContentTitle("sync-3")
+            .setContentText("android kotlin training")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify(123,builder.build())
+
+        /*with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(123, builder.build())}*/
+
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "sync promos chan name"
+                //getString(R.string.channel_name)
+            val descriptionText = "this shows notifications wrt sync promos"
+                //getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("CHANNEL_ID", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 
     private fun downloadImage() {
         var downloadTask = DownloadTask(progressBar)
