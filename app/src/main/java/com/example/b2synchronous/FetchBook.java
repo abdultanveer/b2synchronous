45b29029fbfa7c9420e8bd49cbda468b16deeea6
abdul ansari <abdul.tanveer@gmail.com>
2022-09-13 15:02:32 +0530

    whowrote it -- getting json

---
+++ app/src/main/java/com/example/b2synchronous/FetchBook.java
@@ -0,0 +1,26 @@
+package com.example.b2synchronous;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+public class FetchBook  extends AsyncTask<String,Void,String> {
+    private TextView mTitleText;
+    private TextView mAuthorText;
+
+
+    public FetchBook(TextView mTitleText, TextView mAuthorText) {
+        this.mTitleText = mTitleText;
+        this.mAuthorText = mAuthorText;
+    }
+
+
+    @Override
+    protected String doInBackground(String... bookName) {
+        return NetworkUtils.getBookInfo(bookName[0]);
+    }
+
+    @Override
+    protected void onPostExecute(String json) {
+        super.onPostExecute(json);
+    }
+}
--- app/src/main/java/com/example/b2synchronous/HomeActivity.java
+++ app/src/main/java/com/example/b2synchronous/HomeActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 public class HomeActivity extends AppCompatActivity {
     EditText etContact;
     TextView tvHome;
+    TextView tvAuthor;
     public static String TAG = HomeActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +20,18 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         etContact = findViewById(R.id.etContact);
         tvHome = findViewById(R.id.tvHome);
+        tvAuthor = findViewById(R.id.tvAuthor);
 
       /* String name = getIntent().getExtras().getString("nkey");
        tvHome.setText("hello "+name);*/
     }
 
     public void handleClicks(View viewClicked) {
+       // sendContactData();
+        searchBooks();
+    }
+
+    private void sendContactData() {
         //get the data to be sent
         String contactData = etContact.getText().toString();
         //send the data to the parent activity
@@ -33,6 +40,12 @@ public class HomeActivity extends AppCompatActivity {
         setResult(RESULT_OK,dIntent); //smell-headsup to inform the parent activity
         //close this activity
         finish();
+    }
+
+
+    public void searchBooks(){
+        String queryString = etContact.getText().toString();
+        new FetchBook(tvHome,tvAuthor).execute(queryString);
 
     }
 }
