66517e1091bb67fb2cd2c8006416d9992711f509
abdul ansari <abdul.tanveer@gmail.com>
2022-09-15 10:11:36 +0530

    shared prefs

---
+++ app/src/main/java/com/example/b2synchronous/CalActivity.java
@@ -0,0 +1,14 @@
+package com.example.b2synchronous;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cal);
+    }
+}
--- app/src/main/java/com/example/b2synchronous/MainActivity.kt
+++ app/src/main/java/com/example/b2synchronous/MainActivity.kt
@@ -17,6 +17,8 @@ import com.google.firebase.messaging.FirebaseMessaging
 class MainActivity : AppCompatActivity() {
     lateinit var etName : EditText //declaration
     lateinit var tvMain: TextView
+    lateinit var etPaswd: EditText
+
     var TAG = MainActivity::class.java.simpleName
 
 
@@ -25,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main) //inflated -layoutinflater
         etName = findViewById(R.id.etName)
         tvMain = findViewById(R.id.tvMain)
+        etPaswd = findViewById(R.id.etPassword)
         Log.i(TAG,"im in oncreate")
     }
 
@@ -38,12 +41,40 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         Log.i(TAG,"resuming-restore state")
+        restoreData()
 
+    }
+
+    private fun restoreData() {
+        //get the file and open it
+        var sharedprefs = getSharedPreferences("preferencesSync", MODE_PRIVATE)
+        //read from the file
+        var name = sharedprefs.getString("namekey","")
+        var pass = sharedprefs.getString("passkey","")
+        //restore the data into edittexts
+        etName.setText(name)
+        etPaswd.setText(pass)
     }
 
     override fun onPause() {
         super.onPause()
         Log.i(TAG,"pausing-save state")
+        saveData()
+    }
+
+    private fun saveData() {
+        //get the data from the edittexts
+        var name = etName.text.toString()
+        var pass = etPaswd.text.toString()
+        //create a file
+        var sharedprefs = getSharedPreferences("preferencesSync", MODE_PRIVATE)
+        //open the file in edit mode
+        var editor = sharedprefs.edit()
+        //write to the file
+        editor.putString("namekey",name)
+        editor.putString("passkey",pass)
+        //save the file
+        editor.apply()
     }
 
     override fun onStop() {
