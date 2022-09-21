c7006cf848758ed71bb12236dcd1641d1a54217d
abdul ansari <abdul.tanveer@gmail.com>
2022-09-21 14:56:36 +0530

    setonitemclicklistener on listview within fragment

--- app/src/main/java/com/example/b2synchronous/HeadlinesFragment.kt
+++ app/src/main/java/com/example/b2synchronous/HeadlinesFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
-class HeadlinesFragment: Fragment() {
+class HeadlinesFragment: Fragment(), AdapterView.OnItemClickListener {
+    lateinit var headlinesListView: ListView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +19,13 @@ class HeadlinesFragment: Fragment() {
     ): View? {
          super.onCreateView(inflater, container, savedInstanceState)
         var view = inflater.inflate(R.layout.fragment_headlines,container,false)
+        headlinesListView = view.findViewById(R.id.headlinesListview)
+        headlinesListView.setOnItemClickListener(this)
         return view
+    }
+
+    override fun onItemClick(adapterview: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+        var itemClicked : String = adapterview?.getItemAtPosition(position).toString()
+        Toast.makeText(context,itemClicked,Toast.LENGTH_SHORT).show()
     }
 }
