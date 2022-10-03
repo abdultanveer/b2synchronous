de2c8ed10661e471ac4cd8827b8b0b01a90d2889
abdul ansari <abdul.tanveer@gmail.com>
2022-10-03 10:32:51 +0530

    webview-assets

--- app/src/main/java/com/example/b2synchronous/NewsContentFragment.java
+++ app/src/main/java/com/example/b2synchronous/NewsContentFragment.java
@@ -1,14 +1,17 @@
 package com.example.b2synchronous;
 
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.webkit.WebViewAssetLoader;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -16,15 +19,40 @@ public class NewsContentFragment extends Fragment {
 
 
 TextView tvContent;
+WebView webView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
          super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_news_content, container,false);
         tvContent = view.findViewById(R.id.tvNewsContent);
+        webView = view.findViewById(R.id.webView);
         return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+       // webView.loadUrl("https://google.com");
+       /* String unencodedHtml =
+                "<html><body>'%23' is the percent code for ‘#‘ </body></html>";
+        String encodedHtml = Base64.encodeToString(unencodedHtml.getBytes(),
+                Base64.NO_PADDING);
+        webView.loadData(encodedHtml, "text/html", "base64");*/
+
+        loadHtmlAssets();
+
+
+    }
+
+    private void loadHtmlAssets() {
+        final WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
+                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(getContext()))
+                .addPathHandler("/res/", new WebViewAssetLoader.ResourcesPathHandler(getContext()))
+                .build();
+        webView.setWebViewClient(new LocalContentWebViewClient(assetLoader));
+        webView.loadUrl("https://appassets.androidplatform.net/assets/index.html");
+    }
 
     public void setHeadline(@NotNull String headline) {
         tvContent.setText(headline);
